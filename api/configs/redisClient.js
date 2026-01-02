e31d43e6065923ed2d217dcb9484a14d5dfce4e9
Pedro Henrique <henriquepedro1912@gmail.com>
2026-01-02 14:49:15 -0300

    correção client redis

--- api/configs/redisClient.js
+++ api/configs/redisClient.js
@@ -4,6 +4,7 @@ const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     password: process.env.REDIS_PASSWORD || undefined,
+    enableReadyCheck: false,
     retryStrategy: times => Math.min(times * 50, 2000)
 });
 
